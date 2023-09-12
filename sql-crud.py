42f119f69723fb1ed0e4ae24355172bed4c2e1b4
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-09-12 08:14:35 +0000

    Final commit

--- sql-crud.py
+++ sql-crud.py
@@ -11,13 +11,24 @@ base = declarative_base()
 
 
 # create a class-based model for the "Programmer" table
-class Programmer(base):
-    __tablename__ = "Programmer"
+# class Programmer(base):
+#     __tablename__ = "Programmer"
+#     id = Column(Integer, primary_key=True)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     gender = Column(String)
+#     nationality = Column(String)
+#     famous_for = Column(String)
+
+
+# create a class-based model for the "Clinic" table
+class Clinic(base):
+    __tablename__ = "Clinic"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    gender = Column(String)
-    nationality = Column(String)
+    company_name = Column(String)
+    company_type = Column(String)
+    owner = Column(String)
+    city = Column(String)
     famous_for = Column(String)
 
 
@@ -31,62 +42,72 @@ session = Session()
 base.metadata.create_all(db)
 
 
+# creating records on our Clinic table
+Plastikmottagningen = Clinic(
+    company_name="Plastiken",
+    company_type="Private",
+    owner="Karolinska",
+    city="Stockholm",
+    famous_for="saving lives"
+)
+
+
 # creating records on our Progammer table
-ada_lovelace = Programmer(
-    first_name="Ada",
-    last_name="Lovelace",
-    gender="F",
-    nationality="British",
-    famous_for="First Programmer"
-)
+# ada_lovelace = Programmer(
+#     first_name="Ada",
+#     last_name="Lovelace",
+#     gender="F",
+#     nationality="British",
+#     famous_for="First Programmer"
+# )
 
-alan_turing = Programmer(
-    first_name="Alan",
-    last_name="Turing",
-    gender="M",
-    nationality="British",
-    famous_for="Modern Computing"
-)
+# alan_turing = Programmer(
+#     first_name="Alan",
+#     last_name="Turing",
+#     gender="M",
+#     nationality="British",
+#     famous_for="Modern Computing"
+# )
 
-grace_hopper = Programmer(
-    first_name="Grace",
-    last_name="Hopper",
-    gender="F",
-    nationality="American",
-    famous_for="COBOL language"
-)
+# grace_hopper = Programmer(
+#     first_name="Grace",
+#     last_name="Hopper",
+#     gender="F",
+#     nationality="American",
+#     famous_for="COBOL language"
+# )
 
-margaret_hamilton = Programmer(
-    first_name="Margaret",
-    last_name="Hamilton",
-    gender="F",
-    nationality="American",
-    famous_for="Apollo 11"
-)
+# margaret_hamilton = Programmer(
+#     first_name="Margaret",
+#     last_name="Hamilton",
+#     gender="F",
+#     nationality="American",
+#     famous_for="Apollo 11"
+# )
 
-bill_gates = Programmer(
-    first_name="Bill",
-    last_name="Gates",
-    gender="M",
-    nationality="American",
-    famous_for="Microsoft"
-)
+# bill_gates = Programmer(
+#     first_name="Bill",
+#     last_name="Gates",
+#     gender="M",
+#     nationality="American",
+#     famous_for="Microsoft"
+# )
 
-tim_berners_lee = Programmer(
-    first_name="Tim",
-    last_name="Berners-Lee",
-    gender="M",
-    nationality="British",
-    famous_for="World Wide Web"
-)
+# tim_berners_lee = Programmer(
+#     first_name="Tim",
+#     last_name="Berners-Lee",
+#     gender="M",
+#     nationality="British",
+#     famous_for="World Wide Web"
+# )
 
-debbie_bergstrom = Programmer(
-    first_name="Debbie",
-    last_name="Bergstrom",
-    gender="F",
-    nationality="Swedish",
-    famous_for="being awesome"
-)
+# debbie_bergstrom = Programmer(
+#     first_name="Debbie",
+#     last_name="Bergstrom",
+#     gender="F",
+#     nationality="Swedish",
+#     famous_for="being awesome"
+# )
 
 # add each instance of our programmers to our session
 # session.add(ada_lovelace)
@@ -95,20 +116,58 @@ debbie_bergstrom = Programmer(
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(debbie_bergstrom)
+# session.add(debbie_bergstrom)
+session.add(Plastikmottagningen)
 
 
-#commit our session to the database
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
+
+# commit our session to the database
 session.commit()
 
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(
+#     first_name=fname, last_name=lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
+
 # query the database to find all Programmers
-programmers = session.query(Programmer)
-for programmer in programmers:
-    print(
-        programmer.id,
-        programmer.first_name + " " + programmer.last_name,
-        programmer.gender,
-        programmer.nationality,
-        programmer.famous_for,
-        sep=" | "
-    )
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     print(
+#         programmer.id,
+#         programmer.first_name + " " + programmer.last_name,
+#         programmer.gender,
+#         programmer.nationality,
+#         programmer.famous_for,
+#         sep=" | "
+#     )
